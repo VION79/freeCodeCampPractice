cd5ca1c3cb6fac1e391840b7d41acbbb5f39456e
VION79 <vion79@gmail.com>
2022-11-28 12:16:35 -0500

    js

--- practice.js
+++ practice.js
@@ -1,4 +1,35 @@
-
+/*
 const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+
+document.write (myStr); */
+
+/* const myStr = '<a href="http://www.kmjgmg.com" target="_blank">Link</a>'; */
+
+/* const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; */
+
+/* const myStr = "This is the start." + " This is the end."; */
+
+/* Constructing Strings with Variables 
+const myName = "Myung";
+const myStr = "My name is " + myName + " and I am well!";
+*/
+
+/*Appending Variables to Strings
+
+const someAdjective = "cool";
+let myStr = "Learning to code is ";
+myStr += someAdjective;
+
+*/
+
+/*Find the Length of a String
+
+let lastNameLength = 0;
+const lastName = "Lovelace";
+lastNameLength = lastName.length;
+console.log(lastNameLength);
+
+*/
+
 
 document.write (myStr);
