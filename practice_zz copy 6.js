44d5a850ba6cf67191d05942b20600e9cf0ef5aa
VION79 <vion79@gmail.com>
2022-12-01 16:37:08 -0500

    using recursion

---
+++ practice_zz copy 6.js
@@ -0,0 +1,20 @@
+/* Problem
+
+
+*/
+
+// Setup
+
+
+
+// change code above this line
+
+console.log();
+
+/* Node.js result in the terminal
+
+C:\Users\Acer-aspire-es1\githubrepo_acer\freeCodeCampPractice>node practice_86.js 
+[ 1, 2, 3, 4, 5 ]
+
+*/
+
